d0198cb179472d89bf36e91ebaaac694d8b4ad54
SimonVrouwe <simonvrouwe@yahoo.com>
2017-09-02 23:05:22 +0300

    merge

--- aGoeie.cpp
+++ aGoeie.cpp
@@ -11,10 +11,7 @@ int main(){
 	std::cout << "*pi: " << *pi << std::endl;
 	std::cout << "A goeie"<<std::endl;
 <<<<<<< HEAD
-	std::map<std::string,int> age_m = { {"Floor",17}, {"Piet",35}, {"Rosa",28}, {"Henk",84} };
-=======
-	std::map<std::string,int> age_m = { {"Stella",42}, {"Piet",35}, {"Rosa",28}, {"Henk",84} };
->>>>>>> 935711feccb00c0263065e9915abbe2d6f09cab8
+	std::map<std::string,int> age_m = { {"Floor",17}, {"Piet",35}, {"Rosa",28}, {"Henk",84}, {"Stella",42} };
 	for (auto elem : age_m)
 		cout << elem.first << " is " << elem.second << " years old." << endl;
 }
