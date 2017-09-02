9d4bc176760d76b1cec39039ecbe852225733ce5
Simon Vrouwe <simonvrouwe@yahoo.com>
2017-09-02 14:57:05 +0300

    added some shared ptr for testing

--- aGoeie.cpp
+++ aGoeie.cpp
@@ -1,5 +1,9 @@
+#include <memory>
 #include <iostream>
 
 int main(){
-	std::cout<<"A goeie"<<std::endl;
+	auto pi = std::make_shared<int>(10);
+	std::cout << "pi: " << pi << std::endl;
+	std::cout << "*pi: " << *pi << std::endl;
+	std::cout << "A goeie"<<std::endl;
 }
