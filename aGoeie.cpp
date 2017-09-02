3bb47904b3872844178ae2d476851c8b86bfd93c
Simon Vrouwe <simonvrouwe@yahoo.com>
2017-09-02 15:08:43 +0300

    added some fancy map and for loop, and test the --amend option

--- aGoeie.cpp
+++ aGoeie.cpp
@@ -1,9 +1,16 @@
 #include <memory>
 #include <iostream>
+#include <map>
+#include <string>
+using std::cout;
+using std::endl;
 
 int main(){
 	auto pi = std::make_shared<int>(10);
 	std::cout << "pi: " << pi << std::endl;
 	std::cout << "*pi: " << *pi << std::endl;
 	std::cout << "A goeie"<<std::endl;
+	std::map<std::string,int> age_m = { {"Piet",35}, {"Rosa",28}, {"Henk",84} };
+	for (auto elem : age_m)
+		cout << elem.first << " is " << elem.second << " years old." << endl;
 }
